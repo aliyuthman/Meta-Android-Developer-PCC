569524cfe9f220306598d957429c6932a731a58b
Aliyu Usman <aliyuusman64@gmail.com>
2023-09-17 13:19:52 +0100

    demonstration of creating grid cell, and grid list

---
+++ C5-Create-The-User-Interface-In-Android-Studio/W3-Advanced-UI-With-Jetpack-Compose/M2-Lists-And-Grids/L2-Defining-A-Grid/app/src/main/java/com/example/app5322/MenuGrid.kt
@@ -0,0 +1,75 @@
+package com.example.app5322
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+
+
+@Composable
+fun MenuGrid() {
+
+    GridCell()
+    
+}
+
+@Composable
+fun GridCell() {
+
+    Card(
+        elevation = CardDefaults.cardElevation(16.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+
+        Box(
+            modifier = Modifier
+                .requiredSize(180.dp, 180.dp)
+                .padding(8.dp)
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.greeksalad),
+                contentDescription = "Greek Salad"
+            )
+
+            Text(
+                text = "Greek Salad",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .background(Color.White)
+                    .align(Alignment.TopStart)
+            )
+
+            Text(
+                text = "$12.99", fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(Color.White)
+                    .align(Alignment.BottomEnd)
+            )
+
+        }
+
+    }
+
+}
