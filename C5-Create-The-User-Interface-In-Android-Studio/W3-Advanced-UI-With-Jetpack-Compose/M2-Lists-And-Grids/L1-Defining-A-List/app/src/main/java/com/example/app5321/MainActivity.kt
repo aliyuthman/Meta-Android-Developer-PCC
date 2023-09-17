c4ee9313061d5eb4ff7987fd2c38af451ca884dc
Aliyu Usman <aliyuusman64@gmail.com>
2023-09-17 12:54:46 +0100

    creating vertical and horizontal list

--- C5-Create-The-User-Interface-In-Android-Studio/W3-Advanced-UI-With-Jetpack-Compose/M2-Lists-And-Grids/L1-Defining-A-List/app/src/main/java/com/example/app5321/MainActivity.kt
+++ C5-Create-The-User-Interface-In-Android-Studio/W3-Advanced-UI-With-Jetpack-Compose/M2-Lists-And-Grids/L1-Defining-A-List/app/src/main/java/com/example/app5321/MainActivity.kt
@@ -3,44 +3,73 @@ package com.example.app5321
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.app5321.ui.theme.App5321Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App5321Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+            Column(
+//                modifier = Modifier.fillMaxWidth(), //use for row
+                modifier = Modifier.fillMaxHeight(), //use for row
+//                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalArrangement = Arrangement.Center,
+            ) {
+
+                Categories.forEach {
+                    MenuCategory(category = it)
                 }
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    App5321Theme {
-        Greeting("Android")
+    @Composable
+    fun MenuCategory(category: String) {
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color.LightGray),
+            modifier = Modifier.padding(5.dp)
+        ) {
+
+            Text(
+                text = category,
+                color = Color.DarkGray,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun MenuCategoryPreview() {
+        MenuCategory(category = "Category")
     }
 }
+
+val Categories = listOf<String>(
+    "Lunch",
+    "Dessert",
+    "Mains"
+)
